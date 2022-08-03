c74a7f703186452da5104a957b7fbaec0e13f842
Megha Bodke <megha.n.bodke@gmail.com>
2022-08-03 14:46:02 +0530

    close btn functionality added

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const hamburgericon = document.getElementById('hmbgbtn');
-const navbar = document.querySelector('.navbar');
-const morebtn = document.getElementById('morebtn');
-const lessbtn = document.getElementById('lessbtn');
-const partnercontainer = document.querySelector('.partnercontainer');
+const hamburgericon = document.getElementById("hmbgbtn");
+const closebtn = document.getElementById("closebtn");
+const navbar = document.querySelector(".navbar");
+const morebtn = document.getElementById("morebtn");
+const lessbtn = document.getElementById("lessbtn");
+const partnercontainer = document.querySelector(".partnercontainer");
 
-hamburgericon.addEventListener('click', () => {
-  navbar.classList.toggle('show');
+hamburgericon.addEventListener("click", () => {
+  navbar.classList.toggle("show");
 });
 
-morebtn.addEventListener('click', () => {
-  partnercontainer.classList.toggle('show');
-  morebtn.style.display = 'none';
+closebtn.addEventListener("click", function () {
+  navbar.classList.toggle("show");
+});
+morebtn.addEventListener("click", () => {
+  partnercontainer.classList.toggle("show");
+  morebtn.style.display = "none";
 });
 
-lessbtn.addEventListener('click', () => {
-  partnercontainer.classList.toggle('show');
+lessbtn.addEventListener("click", () => {
+  partnercontainer.classList.toggle("show");
 });
