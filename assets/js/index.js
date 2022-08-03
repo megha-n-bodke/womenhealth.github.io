80e3f8fa39f47a07cfd6d4a389ffbb6fd38346da
Megha Bodke <megha.n.bodke@gmail.com>
2022-08-03 21:12:36 +0530

    fixed linter errors

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,28 @@
-const hamburgericon = document.getElementById("hmbgbtn");
-const closebtn = document.getElementById("closebtn");
-const navbar = document.querySelector(".navbar");
-const morebtn = document.getElementById("morebtn");
-const lessbtn = document.getElementById("lessbtn");
-const partnercontainer = document.querySelector(".partnercontainer");
-const footer = document.getElementById("footer");
+const hamburgericon = document.getElementById('hmbgbtn');
+const closebtn = document.getElementById('closebtn');
+const navbar = document.querySelector('.navbar');
+const morebtn = document.getElementById('morebtn');
+const lessbtn = document.getElementById('lessbtn');
+const partnercontainer = document.querySelector('.partnercontainer');
+const footer = document.getElementById('footer');
 
-hamburgericon.addEventListener("click", () => {
-  navbar.classList.toggle("show");
+hamburgericon.addEventListener('click', () => {
+  navbar.classList.toggle('show');
 });
 
-closebtn.addEventListener("click", function () {
-  navbar.classList.toggle("show");
+closebtn.addEventListener('click', () => {
+  navbar.classList.toggle('show');
 });
-morebtn.addEventListener("click", () => {
-  partnercontainer.classList.toggle("show");
-  morebtn.style.display = "none";
-  footer.style.display = "flex";
-  lessbtn.style.display = "flex";
+morebtn.addEventListener('click', () => {
+  partnercontainer.classList.toggle('show');
+  morebtn.style.display = 'none';
+  footer.style.display = 'flex';
+  lessbtn.style.display = 'flex';
 });
 
-lessbtn.addEventListener("click", () => {
-  partnercontainer.classList.toggle("show");
-  morebtn.style.display = "block";
-  footer.style.display = "none";
-  lessbtn.style.display = "none";
+lessbtn.addEventListener('click', () => {
+  partnercontainer.classList.toggle('show');
+  morebtn.style.display = 'block';
+  footer.style.display = 'none';
+  lessbtn.style.display = 'none';
 });
