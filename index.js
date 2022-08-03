f6e25f31a459d972bc6dc5df5c68f0dd10f44e36
Megha Bodke <megha.n.bodke@gmail.com>
2022-08-03 10:50:17 +0530

    corrected linter errors

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const hamburgericon = document.getElementById("hmbgbtn");
-const navbar = document.querySelector(".navbar");
-const morebtn = document.getElementById("morebtn");
-const lessbtn = document.getElementById("lessbtn");
-const partnercontainer = document.querySelector(".partnercontainer");
+const hamburgericon = document.getElementById('hmbgbtn');
+const navbar = document.querySelector('.navbar');
+const morebtn = document.getElementById('morebtn');
+const lessbtn = document.getElementById('lessbtn');
+const partnercontainer = document.querySelector('.partnercontainer');
 
-hamburgericon.addEventListener("click", function () {
-  navbar.classList.toggle("show");
+hamburgericon.addEventListener('click', () => {
+  navbar.classList.toggle('show');
 });
 
-morebtn.addEventListener("click", function () {
-  partnercontainer.classList.toggle("show");
-  morebtn.style.display = "none";
+morebtn.addEventListener('click', () => {
+  partnercontainer.classList.toggle('show');
+  morebtn.style.display = 'none';
 });
 
-lessbtn.addEventListener("click", function () {
-  partnercontainer.classList.toggle("show");
+lessbtn.addEventListener('click', () => {
+  partnercontainer.classList.toggle('show');
 });
