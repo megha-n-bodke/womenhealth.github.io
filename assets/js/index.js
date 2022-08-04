20fcd0e5a211a18db1615a24ac52de72d3c65a0c
Megha Bodke <megha.n.bodke@gmail.com>
2022-08-04 21:06:55 +0530

    dynamic feature speaker

--- assets/js/index.js
+++ assets/js/index.js
@@ -29,3 +29,66 @@ lessbtn.addEventListener("click", () => {
   footer.style.display = "none";
   lessbtn.style.display = "none";
 });
+
+/* dynamic speakers details */
+
+const speakers = [
+  {
+    name: "Megha",
+    designation: "software devloper",
+    description: "struggling with code ,microverse ,deadline",
+    image: "/images/background.jpeg",
+  },
+  {
+    name: "Aiden",
+    designation: "software devloper",
+    description: "struggling with code ,microverse ,deadline",
+    image: "/images/background.jpeg",
+  },
+  {
+    name: "Saeqa",
+    designation: "software devloper",
+    description: "/images/background.jpeg",
+    image: "/images/background.jpeg",
+  },
+  {
+    name: "Aamir",
+    designation: "software devloper",
+    description: "struggling with code ,microverse ,deadline",
+    image: "/images/background.jpeg",
+  },
+  {
+    name: "Ely",
+    designation: "software devloper",
+    description: "struggling with code ,microverse ,deadline",
+    image: "/images/background.jpeg",
+  },
+  {
+    name: "Philipe",
+    designation: "software devloper",
+    description: "struggling with code ,microverse ,deadline",
+    image: "/images/background.jpeg",
+  },
+];
+let speaker = "";
+/* div for placing speakers */
+const speakerdiv = document.querySelector(".speakerintro");
+document.addEventListener("DOMContentLoaded", () => {
+  speakers.map((person) => {
+    speaker += `
+      <div class="speakerimage">
+               <img src="${person.image}" alt="speaker1">
+           </div>
+            <div class="speakerdetails">
+                <div class="name">
+                    <h2>${person.name}</h2>
+                </div>
+                <div class="profession">${person.designation}
+                </div>
+                <div class="shortintro">${person.description}
+                </div>
+            </div>`;
+    /* return speaker; */
+  });
+  speakerdiv.innerHTML = speaker;
+});
