2682fcd740ab75330fc1796318ff9b4cae52619b
Megha Bodke <megha.n.bodke@gmail.com>
2022-08-04 16:54:39 +0530

    menu update for desktop version

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,31 @@
-const hamburgericon = document.getElementById('hmbgbtn');
-const closebtn = document.getElementById('closebtn');
-const navbar = document.querySelector('.navbar');
-const morebtn = document.getElementById('morebtn');
-const lessbtn = document.getElementById('lessbtn');
-const partnercontainer = document.querySelector('.partnercontainer');
-const footer = document.getElementById('footer');
+const hamburgericon = document.getElementById("hmbgbtn");
+const closebtn = document.getElementById("closebtn");
+const navbar = document.querySelector(".navbar");
+const morebtn = document.getElementById("morebtn");
+const lessbtn = document.getElementById("lessbtn");
+const partnercontainer = document.querySelector(".partnercontainer");
+const footer = document.getElementById("footer");
 
-hamburgericon.addEventListener('click', () => {
-  navbar.classList.toggle('show');
+hamburgericon.addEventListener("click", () => {
+  navbar.classList.toggle("show");
+  hamburgericon.style.display = "none";
+  closebtn.style.display = "block";
 });
 
-closebtn.addEventListener('click', () => {
-  navbar.classList.toggle('show');
+closebtn.addEventListener("click", () => {
+  navbar.classList.toggle("show");
+  hamburgericon.style.display = "block";
 });
-morebtn.addEventListener('click', () => {
-  partnercontainer.classList.toggle('show');
-  morebtn.style.display = 'none';
-  footer.style.display = 'flex';
-  lessbtn.style.display = 'flex';
+morebtn.addEventListener("click", () => {
+  partnercontainer.classList.toggle("show");
+  morebtn.style.display = "none";
+  footer.style.display = "flex";
+  lessbtn.style.display = "flex";
 });
 
-lessbtn.addEventListener('click', () => {
-  partnercontainer.classList.toggle('show');
-  morebtn.style.display = 'block';
-  footer.style.display = 'none';
-  lessbtn.style.display = 'none';
+lessbtn.addEventListener("click", () => {
+  partnercontainer.classList.toggle("show");
+  morebtn.style.display = "block";
+  footer.style.display = "none";
+  lessbtn.style.display = "none";
 });
