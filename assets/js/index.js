63e5388fae4aecacf1d6a1cf2c17f0bc9ec25d97
Megha Bodke <megha.n.bodke@gmail.com>
2022-08-05 12:52:58 +0530

    style changes for desktop view

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,69 +1,70 @@
-const hamburgericon = document.getElementById('hmbgbtn');
-const closebtn = document.getElementById('closebtn');
-const navbar = document.querySelector('.navbar');
-const morebtn = document.getElementById('morebtn');
-const lessbtn = document.getElementById('lessbtn');
-const partnercontainer = document.querySelector('.partnercontainer');
-const footer = document.getElementById('footer');
+const hamburgericon = document.getElementById("hmbgbtn");
+const closebtn = document.getElementById("closebtn");
+const navbar = document.querySelector(".navbar");
+const morebtn = document.getElementById("morebtn");
+const lessbtn = document.getElementById("lessbtn");
+const partnercontainer = document.querySelector(".partnercontainer");
+const footer = document.getElementById("footer");
 
-hamburgericon.addEventListener('click', () => {
-  navbar.classList.toggle('show');
-  hamburgericon.style.display = 'none';
-  closebtn.style.display = 'block';
+hamburgericon.addEventListener("click", () => {
+  navbar.classList.toggle("show");
+  hamburgericon.style.display = "none";
+  closebtn.style.display = "block";
 });
 
-closebtn.addEventListener('click', () => {
-  navbar.classList.toggle('show');
-  hamburgericon.style.display = 'block';
+closebtn.addEventListener("click", () => {
+  navbar.classList.toggle("show");
+  hamburgericon.style.display = "block";
 });
-morebtn.addEventListener('click', () => {
-  partnercontainer.classList.toggle('show');
-  morebtn.style.display = 'none';
-  footer.style.display = 'flex';
-  lessbtn.style.display = 'flex';
+morebtn.addEventListener("click", () => {
+  partnercontainer.classList.toggle("show");
+  morebtn.style.display = "none";
+  footer.style.display = "flex";
+  lessbtn.style.display = "flex";
 });
 
-lessbtn.addEventListener('click', () => {
-  partnercontainer.classList.toggle('show');
-  morebtn.style.display = 'block';
-  footer.style.display = 'none';
-  lessbtn.style.display = 'none';
+lessbtn.addEventListener("click", () => {
+  partnercontainer.classList.toggle("show");
+  morebtn.style.display = "block";
+  footer.style.display = "none";
+  lessbtn.style.display = "none";
 });
 
 /* dynamic speakers details */
 
 const speakers = [
   {
-    name: 'Megha',
-    designation: 'software devloper',
-    description: 'struggling with code ,microverse ,deadline',
-    image: '/images/background.jpeg',
+    name: "Megha",
+    designation: "software devloper",
+    description: "struggling with code ,microverse ,deadline",
+    image: "/images/background.jpeg",
   },
   {
-    name: 'Aiden',
-    designation: 'software devloper',
-    description: 'struggling with code ,microverse ,deadline',
-    image: '/images/background.jpeg',
+    name: "Aiden",
+    designation: "software devloper",
+    description: "struggling with code ,microverse ,deadline",
+    image: "/images/background.jpeg",
   },
   {
-    name: 'Saeqa',
-    designation: 'software devloper',
-    description: '/images/background.jpeg',
-    image: '/images/background.jpeg',
+    name: "Saeqa",
+    designation: "software devloper",
+    description: "/images/background.jpeg",
+    image: "/images/background.jpeg",
   },
   {
-    name: 'Aamir',
-    designation: 'software devloper',
-    description: 'struggling with code ,microverse ,deadline',
-    image: '/images/background.jpeg',
+    name: "Aamir",
+    designation: "software devloper",
+    description: "struggling with code ,microverse ,deadline",
+    image: "/images/background.jpeg",
   },
 ];
-let speaker = '';
+let speaker = "";
 /* div for placing speakers */
-const speakerdiv = document.querySelector('.speakerintro');
-document.addEventListener('DOMContentLoaded', () => {
+const speakerdiv = document.querySelector(".speakerintro");
+document.addEventListener("DOMContentLoaded", () => {
   speakers.map((person) => {
     speaker += `
+    <div class="speakercard">
       <div class="speakerimage">
                <img src="${person.image}" alt="speaker1">
            </div>
@@ -75,6 +76,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 </div>
                 <div class="shortintro">${person.description}
                 </div>
+            </div>
             </div>`;
     return speaker;
   });
